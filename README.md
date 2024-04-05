28241c4c87bad4577e334410e8fbb3b98ce27e2e
alialemana <inf21021@lehre.dhbw-stuttgart.de>
2024-04-05 11:07:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Start flask app
+export FLASK_APP=hello
+export FLASK_ENV=development
+flask run (-p port)
---
+++ static/css/js/script.js
@@ -0,0 +1,3 @@
+const input_file = document.getElementById('image-input');
+const input_label = document.getElementById('image-label');
+const input_preview = document.getElementById('image-preview');
